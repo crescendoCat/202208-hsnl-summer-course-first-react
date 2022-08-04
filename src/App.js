ce0ec89680d2ab49b547cc9fd3fe4ec7d32e6853
crescendoCat <c.crescendo.cat@gmail.com>
2022-08-04 15:39:05 +0800

    feat: Change ui structure a little bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react";
 
 function ListItem(props) {
 
@@ -13,9 +12,9 @@ function ListItem(props) {
 function App() {
   const listItems = ["item1", "item2", "item3"]
   return (
-    <>
-      <div id="title">Todo List</div>
-      <div className="App">
+    <div className="App">
+      <h1 id="title">Todo List</h1>
+      <div>
         <ul>
           {
             listItems.map((item, idx) => 
@@ -23,10 +22,8 @@ function App() {
             )
           }
         </ul>
-        <li>
-        </li>
       </div>
-    </>
+    </div>
   );
 }
 
